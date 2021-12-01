5ff43f910a5c4ad919466ad577ac1201f69deee7
augustin1717 <augustin.oltramare@gmail.com>
2021-12-01 15:18:50 +0100

    delete

--- Setup.R
+++
@@ -1,6 +0,0 @@
-# The setup file loads all packages needed to run the Project.Rmd file 
-
-# load the required packages
-packages <- c("tidyverse", "kableExtra", "shiny", "shinyjs", "unikn", "vembedr", "purrr", "here")
-
-purrr::walk(packages, library, character.only = TRUE)
