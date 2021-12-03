292845fbf59d484467726a2d20b29b0b3ead93e5
augustin1717 <augustin.oltramare@gmail.com>
2021-12-03 15:30:48 +0100

    update first part

--- dsfba_project_1/scripts/setup.R
+++ dsfba_project_1/scripts/setup.R
@@ -6,7 +6,7 @@
 packages <- c(
   "here", # for the project's organization
   "tidyverse", "lubridate","data.table","lubridate", # for wrangling
-  "ggrepel", "gghighlight", "patchwork", "maps", "scales","sp","rgdal","cartogram","tmap","grid","sf","car", # for plotting
+  "ggrepel", "gghighlight", "patchwork", "maps", "scales","sp","rgdal","cartogram","tmap","grid","sf","car","FactoMineR","factoextra","pdp", # for plotting
   "knitr", "kableExtra", "stargazer", "bookdown", "rmarkdown" # for the report
 )
 purrr::walk(packages, library, character.only = TRUE)
